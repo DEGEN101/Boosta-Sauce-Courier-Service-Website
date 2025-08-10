5ca69ba7a688b49f0ee98f985955d0eeb877a4c6
YassirAwad <2623035@students.wits.ac.za>
2025-08-10 17:25:05 +0200

    - Created Form - Added Drop-Shadow Effect

--- src/Pages/ContactPage/ContactPage.jsx
+++ src/Pages/ContactPage/ContactPage.jsx
@@ -1,12 +1,24 @@
+import ContactForm from "../../Components/ContactForm/ContactForm";
 import Footer from "../../Components/Footer/Footer";
+import "./ContactPage.css"
+
 
 const ContactPage = () => {
-    return (
-        <>
-            <h1> Contact </h1>
-            <Footer />
-        </>
-    );
-}
+  return (
+    <>
+      <section className="contact-hero">
+        <h1 className="contact-title">Contact Us</h1>
+        <p className="contact-subtitle">
+          Request a quote by filling out the form below
+        </p>
+        <section className="contact-form">
+            <ContactForm />
+        </section>
+        
+      </section>
+      <Footer />
+    </>
+  );
+};
 
 export default ContactPage;
