350c9abd59969d2d5b75a0584f0e8c6cad3d1c5a
DEGEN101 <ntando.raji13@gmail.com>
2025-08-17 18:35:50 +0200

    Created 2nd header component

--- src/Pages/ContactPage/ContactPage.jsx
+++ src/Pages/ContactPage/ContactPage.jsx
@@ -1,8 +1,10 @@
+import Header2 from "../../Components/Header2/Header2";
 import Footer from "../../Components/Footer/Footer";
 
 const ContactPage = () => {
     return (
-        <>
+        <>  
+            <Header2 />
             <h1> Contact </h1>
             <Footer />
         </>
